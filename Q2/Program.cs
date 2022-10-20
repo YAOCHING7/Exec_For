71fee8730f6a9f5f4513862f8b3732c73b181382
Tim <vunvun0213@gmail.com>
2022-10-21 00:37:36 +0800

    加入專案檔案。

---
+++ Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                string space = new string(' ', rows - i);
+                string stars = new string('*', 2 * i - 1);
+
+                string row = space + stars;
+
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
